a1d347d3cd4f142389cf694bf4993917440e5c99
AnyaGalkina <anna.blackbird1@gmail.com>
2022-06-21 17:02:31 +0700

    Add styles

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import imgError404 from '../../../assets/images/404-error-with-portals-concept-illustration.webp'
+import styles from './Error404.module.css'
 
 function Error404() {
     return (
         <div>
-            <img alt={"404 Page not found!"} src={imgError404}/>
+            <img className={styles.error} alt={"404 Page not found!"} src={imgError404}/>
             {/*<div>404</div>*/}
             {/*<div>Page not found!</div>*/}
             {/*<div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>*/}
