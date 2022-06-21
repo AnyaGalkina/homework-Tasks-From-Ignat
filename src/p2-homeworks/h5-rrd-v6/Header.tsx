7a36960803609abb9208e176b555bdabdaee69d3
AnyaGalkina <anna.blackbird1@gmail.com>
2022-06-21 13:34:28 +0700

    Add NavLinks

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
 
 function Header() {
     return (
         <div>
             // add NavLinks
-
+            {/*<h2 onMouseOver={() => {}}>Navigation</h2>*/}
+            <NavLink to={PATH.PRE_JUNIOR} />
+            <NavLink to={PATH.JUNIOR} />
+            <NavLink to={PATH.JUNIORPLUS} />
         </div>
     )
 }
