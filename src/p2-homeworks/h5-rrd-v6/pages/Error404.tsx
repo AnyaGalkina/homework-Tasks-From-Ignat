18204f8f12cbf4246056525541deb8f81f8dcd22
AnyaGalkina <anna.blackbird1@gmail.com>
2022-06-21 13:27:45 +0700

    Add new component

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import imgError404 from '../../../assets/images/404-error-with-portals-concept-illustration.webp'
 
 function Error404() {
     return (
         <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <img alt={"404 Page not found!"} src={imgError404}/>
+            {/*<div>404</div>*/}
+            {/*<div>Page not found!</div>*/}
+            {/*<div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>*/}
         </div>
     )
 }
